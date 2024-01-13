b956d5015904dc20c72a2ac038172d395ac8e1af
Jonathan Lao <jon.lao@gmail.com>
2024-01-13 10:11:25 -0600

    updated TopicListItem to remove reference to key

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -7,7 +7,7 @@ const TopicListItem = ({ topicData }) => {
   const { id, slug, title } = topicData;
   return (
     <div className="topic-list__item">
-      <span title={`${slug}`} key={id} >{`${title}`}</span>
+      <span title={`${slug}`} >{`${title}`}</span>
     </div>
   );
 };
