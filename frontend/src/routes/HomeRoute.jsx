b3e42dc00fa700bd3689bbe5bc29bafec3c71d6f
Jonathan Lao <jon.lao@gmail.com>
2024-01-13 11:48:42 -0600

    Integrate and display Mock Data

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,6 +5,7 @@ import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
+
 const HomeRoute = () => {
   return (
     <div className="home-route">
