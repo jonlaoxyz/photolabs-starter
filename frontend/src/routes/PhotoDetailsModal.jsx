6057a626e53619f148a112d1b50920c1b2e4a52b
Jonathan Lao <jon.lao@gmail.com>
2024-01-14 18:57:32 -0600

    cleaned up the code as per suggestion

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,13 +4,11 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({ closeModal }) => {
-  const handleCloseClick = () => {
-    closeModal();
-  };
+
 
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={handleCloseClick} >
+      <button className="photo-details-modal__close-button" onClick={closeModal} >
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
