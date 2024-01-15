89f66e42d7c22cdd1515fc7deb2abfc3bedb9f2a
Jonathan Lao <jon.lao@gmail.com>
2024-01-15 12:59:16 -0600

    clean out console.log

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -10,10 +10,8 @@ const initialState = {
 const reducer = (state, action) => {
   switch (action.type) {
     case 'OPEN_MODAL':
-      console.log('Opening modal with photo:', action.payload);
       return { ...state, isModalOpen: true, selectedPhoto: action.payload };
     case 'CLOSE_MODAL':
-      console.log('Closing modal');
       return { ...state, isModalOpen: false, selectedPhoto: null };
     case 'TOGGLE_FAVORITE':
       const photoId = action.payload;
@@ -21,7 +19,6 @@ const reducer = (state, action) => {
       const newFavoritePhotos = isFavorite
         ? state.favoritePhotos.filter((id) => id !== photoId)
         : [...state.favoritePhotos, photoId];
-      console.log('Toggling favorite for photoId:', photoId);
       return { ...state, favoritePhotos: newFavoritePhotos };
     default:
       return state;
